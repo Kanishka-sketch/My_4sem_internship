82ca9e4c45de182fb197ce71bedda04094f7caeb
Kanishka-sketch <kanishkajoshi206@gmail.com>
2026-06-02 09:58:09 +0530

    Assign12 done.

---
+++ module_class/call_greet.py
@@ -0,0 +1,11 @@
+import pydict
+import greet
+greet.greeting("Kanishka")
+
+a=greet.sum(20,30);
+print("Sum:",a)
+
+greet.greeting(pydict.p['Name']);
+
+
+# no of combibnations,platform.system,datetime module
